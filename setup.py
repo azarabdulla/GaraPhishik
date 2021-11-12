88e60385e125c9941fc19396db6ddf2b75675570
Azar Abdulla Nariman <94136793+azarabdulla@users.noreply.github.com>
2021-11-12 13:03:04 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('sudo apt install php curl python-is-python3 python3 python3-pip')
+os.system('pip3 install pyfiglet')
+os.system('sudo unzip ngrok.zip')
+os.system('sudo chmod +x ngrok')
+os.system('sudo mv ngrok /usr/bin')
+os.system('ngrok')
